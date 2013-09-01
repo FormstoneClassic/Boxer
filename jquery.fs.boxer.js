0a963141b5491027e35a706d61b706839e5ed472
Ben Plum <mr@benplum.com>
2013-08-31 21:16:08 -0400

    Adding dedicated mobile support for images, galleries and videos

--- jquery.fs.boxer.js
+++ jquery.fs.boxer.js
@@ -1,7 +1,7 @@
 /*
  * Boxer [Formstone Library]
  * @author Ben Plum
- * @version 1.6.8
+ * @version 1.7.0
  *
  * Copyright © 2013 Ben Plum <mr@benplum.com>
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
@@ -24,18 +24,22 @@ if (jQuery) (function($) {
 		retina: false,
 		requestKey: "boxer",
 		top: 0,
+		videoRatio: 9 / 16,
 		videoWidth: 600,
 		width: 100
 	};
 	// Internal Data
-	var data = {};
-	var resizeTimer = null;
+	var data = {},
+		resizeTimer = null;
+	
+	// Mobile Detect
+	var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test( (navigator.userAgent||navigator.vendor||window.opera) );
 	
 	// Public Methods
 	var pub = {
 		
 		destroy: function() {
-			_close($.Event("click"));
+			_close();
 			return $(this).off(".boxer");
 		},
 		
@@ -50,13 +54,15 @@ if (jQuery) (function($) {
 				}
 				_open();
 			}
+			
+			return $(this);
 		}
 	};
 	
 	// Initialize
 	function _init(opts) {
 		options.formatter = _formatCaption;
-		return $(this).on("click.boxer", $.extend({}, options, opts || {}), _build);
+		return $(this).on("touchstart.boxer click.boxer", $.extend({}, options, opts || {}), _build);
 	}
 	
 	// Build Boxer
@@ -72,14 +78,14 @@ if (jQuery) (function($) {
 			extension = checkExt[ checkExt.length - 1 ],
 			type = $target.data("type") || "";
 		
-		var is_image    = ( (type == "image") || (extension == "jpeg" || extension == "jpg" || extension == "gif" || extension == "png" || source.substr(0, 10) == "data:image") ),
-			is_video    = ( source.indexOf("youtube.com/embed") > -1 || source.indexOf("player.vimeo.com/video") > -1 ),
-			is_url 		= ( (type == "url") || (!is_image && !is_video && source.substr(0, 4) == "http") ),
-			is_element  = ( (type == "element") || (!is_image && !is_video && !is_url && source.substr(0, 1) == "#") ),
-			is_object   = ( (typeof $object !== "undefined") );
+		var isImage    = ( (type == "image") || (extension == "jpeg" || extension == "jpg" || extension == "gif" || extension == "png" || source.substr(0, 10) == "data:image") ),
+			isVideo    = ( source.indexOf("youtube.com/embed") > -1 || source.indexOf("player.vimeo.com/video") > -1 ),
+			isUrl 		= ( (type == "url") || (!isImage && !isVideo && source.substr(0, 4) == "http") ),
+			isElement  = ( (type == "element") || (!isImage && !isVideo && !isUrl && source.substr(0, 1) == "#") ),
+			isObject   = ( (typeof $object !== "undefined") );
 		
 		// Check if one already exists
-		if ($("#boxer").length < 1 && (is_image || is_video || is_url || is_element || is_object)) {
+		if ($("#boxer").length < 1 && (isImage || isVideo || isUrl || isElement || isObject)) {
 			// Cache internal data
 			data = {
 				$target: $target,
@@ -87,14 +93,19 @@ if (jQuery) (function($) {
 				gallery: {
 					active: false
 				},
-				options: e.data
+				options: e.data,
+				isMobile: (isMobile && !isUrl && !isElement && !isObject)
 			};
 			
-			if (is_image) data.type = "image";
-			else if (is_video) data.type = "video";
-			else data.type = "element";
+			if (isImage) {
+				data.type = "image";
+			} else if (isVideo) {
+				data.type = "video";
+			} else {
+				data.type = "element";
+			}
 			
-			if (is_image || is_video) {
+			if (isImage || isVideo) {
 				// Check for gallery
 				var rel = data.$target.attr("rel");
 				if (typeof rel !== "undefined" && rel !== false) {
@@ -107,12 +118,18 @@ if (jQuery) (function($) {
 			}
 			
 			// Assemble HTML
-			var html = '<div id="boxer-overlay" class="' + data.options.customClass + '" style="opacity: 0"></div>';
+			var html = '';
+			if (!data.isMobile) {
+				html += '<div id="boxer-overlay" class="' + data.options.customClass + '" style="opacity: 0"></div>';
+			}
 			html += '<div id="boxer" class="' + data.options.customClass;
-			if (is_url) {
+			if (data.isMobile) {
+				html += ' mobile';
+			}
+			if (isUrl) {
 				html += ' iframe';
 			}
-			if (is_element || is_object) {
+			if (isElement || isObject) {
 				html += ' inline';
 			}
 			html += '" style="opacity: 0;';
@@ -123,10 +140,12 @@ if (jQuery) (function($) {
 			html += '<span class="boxer-close">Close</span>';
 			html += '<div class="boxer-container" style="opacity: 0; height: ' + data.options.height + 'px; width: ' + data.options.width + 'px">';
 			html += '<div class="boxer-content">';
-			if (is_image || is_video) {
+			if (isImage || isVideo) {
 				html += '<div class="boxer-meta">';
 				
 				if (data.gallery.active) {
+					html += '<div class="boxer-arrow previous">Previous</div>';
+					html += '<div class="boxer-arrow next">Next</div>';
 					html += '<p class="boxer-position"';
 					if (data.gallery.total < 1) { 
 						html += ' style="display: none;"'; 
@@ -134,8 +153,6 @@ if (jQuery) (function($) {
 					html += '>';
 					html += '<span class="current">' + (data.gallery.index + 1) + '</span> of <span class="total">' + (data.gallery.total + 1) + '</span>';
 					html += '</p>';
-					html += '<div class="boxer-arrow previous">Previous</div>';
-					html += '<div class="boxer-arrow next">Next</div>';
 					html += '<div class="boxer-caption gallery">';
 				} else {
 					html += '<div class="boxer-caption">';
@@ -170,43 +187,51 @@ if (jQuery) (function($) {
 			// Bind events
 			$(window).on("resize.boxer", _resize)
 					 .on("keydown.boxer", _keypress);
-			$("body").on("click.boxer", "#boxer-overlay, #boxer .boxer-close", _close);
+			$("body").on("touchstart.boxer click.boxer", "#boxer-overlay, #boxer .boxer-close", _close);
 			if (data.gallery.active) {
-				data.$boxer.on("click.boxer", ".boxer-arrow", _advanceGallery);
+				data.$boxer.on("touchstart.boxer click.boxer", ".boxer-arrow", _advanceGallery);
 			}
 			
 			data.$overlay.stop().animate({ opacity: data.options.opacity }, data.options.duration);
 			data.$boxer.stop().animate({ opacity: 1 }, data.options.duration, function() { 
-				if (is_image) {
+				if (isImage) {
 					_loadImage(source);
-				} else if (is_video) {
+				} else if (isVideo) {
 					_loadVideo(source);
-				} else if (is_url) {
+				} else if (isUrl) {
 					_loadURL(source);
-				} else if (is_element) {
+				} else if (isElement) {
 					_cloneElement(source);
-				} else if (is_object) {
+				} else if (isObject) {
 					_appendObject(data.$object);
 				} else {
 					$.error("BOXER: '" +  source + "' is not valid.");
 				}
 			});
 		}
-		if (is_object) {
+		if (isObject) {
 			return data.$boxer;
 		}
 	}
 	
 	// Open boxer
 	function _open() {
-		var newLeft = ($(window).width() - data.contentWidth - data.padding) / 2;
-		var newTop = (data.options.top <= 0) ? (($(window).height() - data.contentHeight - data.padding) / 2) : data.options.top;
-		if (data.options.fixed !== true) {
-			newTop += $(window).scrollTop();
+		if (data.isMobile) {
+			var newLeft = 0;
+				newTop = 0;
+		} else {
+			var newLeft = ($(window).width() - data.contentWidth - data.padding) / 2,
+				newTop = (data.options.top <= 0) ? (($(window).height() - data.contentHeight - data.padding) / 2) : data.options.top,
+				arrowHeight = data.$arrows.outerHeight();
+			
+			if (data.options.fixed !== true) {
+				newTop += $(window).scrollTop();
+			}
+			
+			data.$arrows.css({ 
+				marginTop: ((data.contentHeight - data.metaHeight - arrowHeight) / 2) 
+			});
 		}
-		
-		var arrowHeight = data.$arrows.outerHeight();
-		data.$arrows.css({ marginTop: ((data.contentHeight - data.metaHeight - arrowHeight) / 2) });
 		
 		data.$boxer.stop().animate({ left: newLeft, top: newTop }, data.options.duration);
 		data.$container.show().stop().animate({ height: data.contentHeight, width: data.contentWidth }, data.options.duration, function(e) {
@@ -220,8 +245,10 @@ if (jQuery) (function($) {
 	
 	// Close boxer
 	function _close(e) {
-		e.preventDefault();
-		e.stopPropagation();
+		if (e.preventDefault) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
 		
 		if (typeof data.$animatables !== "undefined") {
 			data.$animatables.stop().animate({ opacity: 0 }, data.options.duration, function() {
@@ -243,21 +270,33 @@ if (jQuery) (function($) {
 	
 	// Debounce resize events
 	function _resize() {
-		if (resizeTimer !== null) {
-			clearTimeout(resizeTimer);
-			resizeTimer = null;
+		if (!data.isMobile) {
+			if (resizeTimer !== null) {
+				clearTimeout(resizeTimer);
+				resizeTimer = null;
+			}
+			resizeTimer = setTimeout(function() { _center() }, 10);
 		}
-		resizeTimer = setTimeout(function() { _center() }, 10);
 	}
 	
 	// Center boxer on resize
 	function _center() {
-		var newLeft = ($(window).width() - data.$boxer.width() - data.padding) / 2;
-		var newTop = (data.options.top <= 0) ? (($(window).height() - data.$boxer.height() - data.padding) / 2) : data.options.top;
-		if (data.options.fixed !== true) {
-			newTop += $(window).scrollTop();
+		if (data.isMobile) {
+			var newLeft = 0,
+				newTop  = 0;
+		} else {
+			var newLeft = ($(window).width() - data.$boxer.width() - data.padding) / 2,
+				newTop  = (data.options.top <= 0) ? (($(window).height() - data.$boxer.height() - data.padding) / 2) : data.options.top;
+			
+			if (data.options.fixed !== true) {
+				newTop += $(window).scrollTop();
+			}
 		}
-		data.$boxer.css({ left: newLeft, top: newTop });
+		
+		data.$boxer.css({ 
+			left: newLeft, 
+			top:  newTop 
+		});
 	}
 	
 	// Load new image
@@ -266,12 +305,12 @@ if (jQuery) (function($) {
 		data.$image = $("<img />");
 		
 		data.$image.one("load.boxer", function() {
-			data.originalHeight = data.$image[0].height;
-			data.originalWidth = data.$image[0].width;
+			data.naturalHeight = data.$image[0].naturalHeight;
+			data.naturalWidth = data.$image[0].naturalWidth;
 			
 			if (data.options.retina) {
-				data.originalHeight /= 2;
-				data.originalWidth /= 2;
+				data.naturalHeight /= 2;
+				data.naturalWidth /= 2;
 			}
 			
 			data.$content.prepend(data.$image);
@@ -310,7 +349,7 @@ if (jQuery) (function($) {
 		_open();
 	}
 	
-	// Format captions
+	// Format caption
 	function _formatCaption($target) {
 		var title = $target.attr("title");
 		return (title != "" && title !== undefined) ? '<p class="caption">' + title + '</p>' : "";
@@ -318,92 +357,177 @@ if (jQuery) (function($) {
 	
 	// Resize image to fit in viewport
 	function _sizeImage(count) {
-		var height = (count == 0) ? data.originalHeight : data.$image.outerHeight();
-		var width = (count == 0) ? data.originalWidth : data.$image.outerWidth();
-		var metaHeight = (count == 0) ? 0 : data.metaHeight;
-		var windowWidth = $(window).width() - data.options.margin - data.padding;
-		var windowHeight = $(window).height() - data.options.margin - data.padding - metaHeight;
+		data.windowHeight = data.viewportHeight = (count == 0) ? $(window).height() : data.windowHeight;
+		data.windowWidth  = data.viewportWidth = (count == 0) ? $(window).width() : data.windowWidth;
+		
+		data.imageHeight  = (count == 0) ? data.naturalHeight : data.$image.outerHeight();
+		data.imageWidth   = (count == 0) ? data.naturalWidth : data.$image.outerWidth();
+		data.metaHeight   = (count == 0) ? 0 : data.metaHeight;
+		
+		if (count == 0) {
+			data.ratioHorizontal = data.imageHeight / data.imageWidth;
+			data.ratioVertical   = data.imageWidth / data.imageHeight;
+			
+			data.isWide = (data.imageWidth > data.imageHeight);
+		}
 		
 		// Double check min and max
-		if (height < data.options.minHeight) {
-			data.options.minHeight = height;
+		if (data.imageHeight < data.options.minHeight) {
+			data.options.minHeight = data.imageHeight;
 		}
-		if (width < data.options.minWidth) {
-			data.options.minWidth = width;
+		if (data.imageWidth < data.options.minWidth) {
+			data.options.minWidth = data.imageWidth;
 		}
 		
-		var newWidth = width;
-		var newHeight = height;
-		var ratio;
-
-		if (width > height) {
-			ratio = height / width;
-			newWidth = windowWidth;
-			newHeight = newWidth * ratio;
+		if (data.isMobile) {
+			data.viewportHeight -= data.padding;
+			data.viewportWidth  -= data.padding;
 			
-			if (newHeight > windowHeight) {
-				ratio = width / height;
-				newHeight = windowHeight;
-				newWidth = newHeight * ratio;
-			}
+			data.contentHeight = data.viewportHeight;
+			data.contentWidth  = data.viewportWidth;
+			
+			data = _fitImage(data); 
+			
+			data.imageMarginTop  = (data.contentHeight - data.targetImageHeight) / 2;
+			data.imageMarginLeft = (data.contentWidth - data.targetImageWidth) / 2;
 		} else {
-			ratio = width / height;
-			newHeight = windowHeight;
-			newWidth = newHeight * ratio;
+			data.viewportHeight -= data.options.margin + data.padding + data.metaHeight;
+			data.viewportWidth  -= data.options.margin + data.padding;
 			
-			if (newWidth > windowWidth) {
-				ratio = height / width;
-				newWidth = windowWidth;
-				newHeight = newWidth * ratio;
-			}
-		}
-		
-		if (newWidth > width || newHeight > height) {
-			newWidth = width;
-			newHeight = height;
-		}
-		
-		if (newWidth < data.options.minWidth || newHeight < data.options.minHeight) {
-			if (width < height) {
-				ratio = height / width;
-				newWidth = data.options.minWidth;
-				newHeight = newWidth * ratio;
-			} else {
-				ratio = width / height;
-				newHeight = data.options.minHeight;
-				newWidth = newHeight * ratio;
-			}
+			data = _fitImage(data);
+			
+			data.contentHeight = data.targetImageHeight;
+			data.contentWidth  = data.targetImageWidth;
+			
+			data.imageMarginTop = 0;
+			data.imageMarginLeft = 0;
 		}
 		
 		// Modify DOM
-		data.$content.css({ width: newWidth });
-		data.$meta.css({ width: newWidth });
-		data.$image.css({ height: newHeight, width: newWidth });
+		data.$content.css({ 
+			height: (data.isMobile) ? data.contentHeight : "auto",
+			width: data.contentWidth 
+		});
+		data.$meta.css({ 
+			width: data.contentWidth 
+		});
+		data.$image.css({ 
+			height: data.targetImageHeight, 
+			width: data.targetImageWidth,
+			marginTop:  data.imageMarginTop,
+			marginLeft: data.imageMarginLeft
+		});
 		
-		data.metaHeight = data.$meta.outerHeight(true);
-		data.contentWidth = newWidth;
-		data.contentHeight = newHeight + data.metaHeight;
-		
-		if (data.contentHeight > windowHeight && count < 2) {
-			return _sizeImage(count+1);
+		if (!data.isMobile) {
+			data.metaHeight = data.$meta.outerHeight(true);
+			data.contentHeight += data.metaHeight;
+			
+			if (data.contentHeight > data.viewportHeight && count < 2) {
+				return _sizeImage(count+1);
+			}
 		}
+		
 		return true;
+	}
+	
+	// Fit image to viewport
+	function _fitImage(data) {
+		if (data.isWide) {
+			//WIDE
+			data.targetImageWidth  = data.viewportWidth;
+			data.targetImageHeight = data.targetImageWidth * data.ratioHorizontal;
+			
+			if (data.targetImageHeight > data.viewportHeight) {
+				data.targetImageHeight = data.viewportHeight;
+				data.targetImageWidth  = data.targetImageHeight * data.ratioVertical;
+			}
+		} else {
+			//TALL
+			data.targetImageHeight = data.viewportHeight;
+			data.targetImageWidth = data.targetImageHeight * data.ratioVertical;
+			
+			if (data.targetImageWidth > data.viewportWidth) {
+				data.targetImageWidth = data.viewportWidth;
+				data.targetImageHeight = data.targetImageWidth * data.ratioHorizontal;
+			}
+		}
+		
+		// MAX
+		if (data.targetImageWidth > data.imageWidth || data.targetImageHeight > data.imageHeight) {
+			data.targetImageWidth = data.imageWidth;
+			data.targetImageHeight = data.imageHeight;
+		}
+		
+		// MIN
+		if (data.targetImageWidth < data.options.minWidth || data.targetImageHeight < data.options.minHeight) {
+			if (data.imageWidth > data.imageHeight) {
+				data.targetImageHeight = data.options.minHeight;
+				data.targetImageWidth = data.targetImageHeight * data.ratioHorizontal;
+			} else {
+				data.targetImageWidth = data.options.minWidth;
+				data.targetImageHeight = data.tagretImageWidth * data.ratioVertical;
+			}
+		}
+		
+		return data;
 	}
 	
 	// Resize image to fit in viewport
 	function _sizeVideo() {
-		var ratio = 9 / 16;
-		var windowWidth = $(window).width() - data.options.margin - data.padding;
-		var newWidth = (data.options.videoWidth > windowWidth) ? windowWidth : data.options.videoWidth;
-		var newHeight = newWidth * ratio;
-
-		data.$videoWrapper.css({ height: newHeight, width: newWidth });
-		data.$content.css({ width: newWidth });
-		data.$meta.css({ width: newWidth });
+		data.windowHeight = $(window).height() - data.padding;
+		data.windowWidth  = $(window).width() - data.padding;
+		data.videoMarginTop = 0;
+		data.videoMarginLeft = 0;
 		
-		data.metaHeight = data.$meta.outerHeight(true);
-		data.contentWidth = newWidth;
-		data.contentHeight = newHeight + data.metaHeight;
+		if (data.isMobile) {
+			data.$meta.css({ 
+				width: data.windowWidth
+			});
+			data.metaHeight = data.$meta.outerHeight(true);
+			
+			data.contentHeight = data.windowHeight;
+			data.contentWidth  = data.windowWidth;
+			
+			data.videoWidth  = data.windowWidth;
+			data.videoHeight = data.videoWidth * data.options.videoRatio;
+			
+			if (data.videoHeight > data.windowHeight - data.metaHeight) {
+				data.videoHeight = data.windowHeight - data.metaHeight;
+				data.videoWidth  = data.videoHeight * data.options.videoRatio;
+			}
+			
+			data.videoMarginTop = (data.contentHeight - data.videoHeight) / 2;
+			data.videoMarginLeft = (data.contentWidth - data.videoWidth) / 2;
+		} else {
+			data.windowHeight -= data.options.margin;
+			data.windowWidth  -= data.options.margin;
+			
+			data.videoWidth  = (data.options.videoWidth > data.windowWidth) ? data.windowWidth : data.options.videoWidth;
+			data.videoHeight = data.videoWidth * data.options.videoRatio;
+			
+			data.contentHeight = data.videoHeight;
+			data.contentWidth  = data.videoWidth;
+		}
+		
+		data.$content.css({ 
+			height: (data.isMobile) ? data.contentHeight : "auto",
+			width: data.contentWidth 
+		});
+		data.$meta.css({ 
+			width: data.contentWidth 
+		});
+		data.$videoWrapper.css({ 
+			height: data.videoHeight, 
+			width: data.videoWidth,
+			marginTop: data.videoMarginTop,
+			marginLeft: data.videoMarginLeft
+		});
+		
+		if (!data.isMobile) {
+			data.metaHeight = data.$meta.outerHeight(true);
+			data.contentHeight = data.videoHeight + data.metaHeight;
+		}
+		data.contentWidth  = data.videoWidth;
 	}
 	
 	// Advance gallery
@@ -436,9 +560,9 @@ if (jQuery) (function($) {
 				data.$position.find(".current").html(data.gallery.index + 1);
 				
 				var source = data.$target.attr("href"),
-					is_video = ( source.indexOf("youtube.com/embed") > -1 || source.indexOf("player.vimeo.com/video") > -1 );
+					isVideo = ( source.indexOf("youtube.com/embed") > -1 || source.indexOf("player.vimeo.com/video") > -1 );
 				
-				if (is_video) {
+				if (isVideo) {
 					_loadVideo(source);
 				} else {
 					_loadImage(source);
