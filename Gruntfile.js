579a168e3d91294e8ad141fe1ba3f0a770c7f6a8
Ben Plum <mr@benplum.com>
2014-10-28 11:55:39 -0400

    Bundling license file;

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,7 +85,10 @@ module.exports = function(grunt) {
 		less: {
 			main: {
 				files: {
-					'<%= pkg.codename %>.css': 'src/<%= pkg.codename %>.less'
+					'<%= pkg.codename %>.css': [
+						'src/<%= pkg.codename %>-config.less',
+						'src/<%= pkg.codename %>.less'
+					]
 				}
 			},
 			min: {
@@ -94,14 +97,17 @@ module.exports = function(grunt) {
 					cleancss: true
 				},
 				files: {
-					'<%= pkg.codename %>.min.css': 'src/<%= pkg.codename %>.less'
+					'<%= pkg.codename %>.min.css': [
+						'src/<%= pkg.codename %>-config.less',
+						'src/<%= pkg.codename %>.less'
+					]
 				}
 			}
 		},
 		// Auto Prefixer
 		autoprefixer: {
 			options: {
-				borwsers: [ '> 1%', 'last 5 versions', 'Firefox ESR', 'Opera 12.1', '>= ie 8' ]
+				browsers: [ '> 1%', 'last 5 versions', 'Firefox ESR', 'Opera 12.1', 'IE 8', 'IE 9' ]
 			},
 			no_dest: {
 				 src: '*.css'
@@ -122,7 +128,7 @@ module.exports = function(grunt) {
 				]
 			}
 		},
-		//Bower sync
+		// Bower sync
 		sync: {
 			all: {
 				options: {
@@ -136,10 +142,32 @@ module.exports = function(grunt) {
 					}
 				}
 			}
+		},
+		// Watcher - For dev only!!
+		watch: {
+			scripts: {
+				files: [
+					'src/**.js'
+				],
+				tasks: [
+					'jshint',
+					'copy'
+				]
+			},
+			styles: {
+				files: [
+					'src/**.less'
+				],
+				tasks: [
+					'less',
+					'autoprefixer'
+				]
+			}
 		}
 	});
 
 	// Load tasks
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -167,7 +195,35 @@ module.exports = function(grunt) {
 		grunt.log.writeln('File "' + destination + '" created.');
 	});
 
+	grunt.registerTask('buildLicense', 'Build Formstone LICENSE.md file.', function () {
+		var pkg = grunt.file.readJSON('package.json'),
+			destination = "LICENSE.md",
+			markdown = 'The MIT License (MIT) \n\n' +
+					   'Copyright ' + grunt.template.today("yyyy") + ' ' + pkg.author.name + ' \n\n' +
+					   'Permission is hereby granted, free of charge, to any person obtaining a copy \n' +
+					   'of this software and associated documentation files (the "Software"), to deal \n' +
+					   'in the Software without restriction, including without limitation the rights \n' +
+					   'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell \n' +
+					   'copies of the Software, and to permit persons to whom the Software is \n' +
+					   'furnished to do so, subject to the following conditions: \n\n' +
+					   'The above copyright notice and this permission notice shall be included in \n' +
+					   'all copies or substantial portions of the Software. \n\n' +
+					   'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \n' +
+					   'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, \n' +
+					   'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE \n' +
+					   'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER \n' +
+					   'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, \n' +
+					   'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN \n' +
+					   'THE SOFTWARE.';
+
+		grunt.file.write(destination, markdown);
+		grunt.log.writeln('File "' + destination + '" created.');
+	});
+
+
+
+
 	// Default task.
-	grunt.registerTask('default', [ 'jshint', 'copy', 'uglify', 'jquerymanifest', 'less', 'autoprefixer', 'usebanner', 'sync', 'buildReadme' ]);
+	grunt.registerTask('default', [ 'jshint', 'copy', 'uglify', 'jquerymanifest', 'less', 'autoprefixer', 'usebanner', 'sync', 'buildReadme', 'buildLicense' ]);
 
 };
